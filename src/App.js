1db8161ea3e42b1a606158f93e823a076024e72d
Zantolotov <antoine.duvard@gmail.com>
2018-04-02 12:50:27 +0200

    huge changes App.css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
-import './App.css';
+
 
 
 const business = {
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../Business/Business.css'; 
+import './Business.css'; 
 
 class Business extends React.Component {
   render () {
       return (
       <div className="Business">
-      <div className="image-container">
-        <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
-      </div>
-      <h2>{this.props.business.name}</h2>
+        <div className="image-container">
+        <img src={this.props.business.imageSrc} alt=''/>
+        </div>
+        <h2>{this.props.business.name}</h2>
       <div className="Business-information">
         <div className="Business-address">
           <p>{this.props.business.address}</p>
@@ -17,12 +17,12 @@ class Business extends React.Component {
         </div>
         <div className="Business-reviews">
           <h3>ITALIAN</h3>
-          <h3 className="rating">4.5 stars}</h3>
-          <p>90 reviews</p>
+          <h3 className="rating">{this.props.business.rating} stars</h3>
+          <p>{this.props.business.reviewCount} reviews</p>
         </div>
       </div>
     </div>
-    )
+    );
   }
 };
 
