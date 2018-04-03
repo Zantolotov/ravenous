d855a97750cd09fdd79ae799f4b3f3af6c730741
Zantolotov <antoine.duvard@gmail.com>
2018-04-03 11:55:31 +0200

    ravenous part III

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './BusinessList.css';
+
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render(){
-        return (<div className="BusinessList">
+        return (
+        <div className="BusinessList">
            {
                this.props.businesses.map(function(business){
                 return <Business business={business}/>})
